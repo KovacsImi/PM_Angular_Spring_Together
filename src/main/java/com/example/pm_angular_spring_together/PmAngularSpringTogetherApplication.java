c627d142a0837e9258dc1e4e2e417d19ce89856d
KovácsImi <imre90@gmail.com>
2023-11-24 13:19:09 +0100

    init commit

---
+++ src/main/java/com/example/pm_angular_spring_together/PmAngularSpringTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.pm_angular_spring_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmAngularSpringTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmAngularSpringTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pm_angular_spring_together/PmAngularSpringTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pm_angular_spring_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmAngularSpringTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
